eb1431d649bdaf8629f1157f04f4c636ab845228
iliags <19849002+iliags@users.noreply.github.com>
2025-08-25 23:36:36 -0700

    Create player controller

---
+++ Plugins/GameFeatures/PossessionFeature/Source/PossessionFeatureRuntime/Private/PossessionPlayerController.cpp
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PossessionPlayerController.h"
+
+#include "Character/LyraCharacter.h"
+#include "Character/LyraHeroComponent.h"
+
+void APossessionPlayerController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+	
+	if (const ALyraCharacter* LyraCharacter = GetPawn<ALyraCharacter>())
+	{
+		if (ULyraHeroComponent* HeroComponent = ULyraHeroComponent::FindHeroComponent(LyraCharacter))
+		{
+			if (InputComponent != nullptr)
+			{
+				if (HeroComponent->IsReadyToBindInputs())
+				{
+					HeroComponent->ResetInputs(this);
+				}
+				UE_LOG(LogTemp, Warning, TEXT("%s - %s(): Initializing player input"), *GetName(), *FString(__FUNCTION__));
+				HeroComponent->InitializePlayerInput(InputComponent);
+			}
+		}
+	}
+	
+}
+
+void APossessionPlayerController::OnUnPossess()
+{
+	if (const ALyraCharacter* LyraCharacter = GetPawn<ALyraCharacter>())
+	{
+		if (ULyraHeroComponent* HeroComponent = ULyraHeroComponent::FindHeroComponent(LyraCharacter))
+		{
+			if (InputComponent != nullptr)
+			{
+				HeroComponent->ResetInputs(this);
+			}
+		}
+	}
+	
+	Super::OnUnPossess();
+}
---
+++ Plugins/GameFeatures/PossessionFeature/Source/PossessionFeatureRuntime/Public/PossessionPlayerController.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Player/LyraPlayerController.h"
+#include "PossessionPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class POSSESSIONFEATURERUNTIME_API APossessionPlayerController : public ALyraPlayerController
+{
+	GENERATED_BODY()
+
+public:
+	virtual void OnPossess(APawn* InPawn) override;
+	virtual void OnUnPossess() override;
+};
